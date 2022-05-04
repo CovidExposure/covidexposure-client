68a97147ddf40476a3c0dcab293f9c3d76548abc
Yizhen Liu <imliuyzh@outlook.com>
2022-05-04 14:31:47 -0700

    Add a link to home page in error page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,18 +1,34 @@
-import { Container, Text, Title } from '@mantine/core';
+import { Button, Container, createStyles, Text, Title } from '@mantine/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = createStyles((theme, params, getRef) => ({
+  container: {
+    alignItems: 'flex-start',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '4rem',
+    height: '100vh',
+    justifyContent: 'center',
+  },
+  message: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+  },
+}));
 
 export default function ErrorPage() {
+  let { classes } = useStyles();
   return (
-    <Container
-      sx={(_) => ({
-        alignItems: 'flex-start',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '1rem',
-        height: '100vh',
-        justifyContent: 'center',
-    })}>
-      <Title order={1}>Error</Title>
-      <Text size="md">Apologies, the page you're looking for cannot be found.</Text>
+    <Container className={classes.container}>
+      <div className={classes.message}>
+        <Title order={1}>Error</Title>
+        <Text size="md">Apologies, the page you're looking for cannot be found.</Text>
+      </div>
+
+      <Link to="/">
+        <Button>Home</Button>
+      </Link>
     </Container>
   );
 }
